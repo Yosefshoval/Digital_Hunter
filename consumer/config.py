5ba579bb22c23c9f767cde66204493aa070944af
Yosef shoval <ys0542522504@gmail.com>
2026-03-16 16:51:24 +0200

    add consumer service in docker compose

--- consumer/config.py
+++ consumer/config.py
@@ -11,7 +11,7 @@ class ConsumerConfig:
     KAFKA_URL = os.getenv("KAFKA_URL", "localhost:9092"),
 
     consumer_config = {
-        "bootstrap.servers": "localhost:9092",
+        "bootstrap.servers": KAFKA_URL,
         "group.id": "main_tracker",
         "auto.offset.reset": "earliest"
     }
